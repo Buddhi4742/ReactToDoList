edd097f43fd3168710e94df62cf2669bf377c803
Buddhi47420 <chathuranga4742@gmail.com>
2023-05-31 09:56:45 +0530

    Added checkboxes and adding tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,48 @@ import "./styles.css";
 
 export default function App(){
   const [newItem,setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
   // setNewItem("hibjf"); //send the DOM to an infinite loop
+  
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTodos((currentTodos) => {
+      return  [ ...currentTodos,
+        {id:crypto.randomUUID(), 
+          title: newItem, 
+          completed: false
+        },
+      ]
+    })
+    // setTodos((currentTodos) => {
+    //   return  [ ...currentTodos,
+    //     {id:crypto.randomUUID(), 
+    //       title: newItem, 
+    //       completed: false
+    //     },
+    //   ]
+    // })
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
 
   return (
   <>
-  <form className="new-item-form"> 
+  <form onSubmit={handleSubmit} className="new-item-form"> 
   <div className="form-row">
+
+    {/* {JSON.stringify(todos)} */}
+
   <label htmlFor="item">New Item</label>
   <input 
     value={newItem} 
@@ -17,22 +53,24 @@ export default function App(){
     id="item">
   </input>
   </div>
-  <buttonm className="btn">Add</buttonm>
+  <button className="btn">Add</button>
   </form>
   <h1 className="header">ToDo List</h1>
   <ul className="list">
-    <li>
-      <label>
-        <inpuit type="checkbox"/>
-        Item 1
-      </label>
-    </li>
-    <li>
-      <label>
-        <inpuit type="checkbox"/>
-        Item 2
-      </label>
-    </li>
+    {todos.map(todo => {
+      return(
+        <li key={todo.id}>
+          <label>
+            <input 
+            type="checkbox" 
+            checked={todo.completed}
+            onChange={e => toggleTodo(todo.id,e.target.checked)}/>
+            {todo.title}
+          </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      )
+    })}
   </ul>
   </>
   )
